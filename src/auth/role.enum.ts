a6c3ca0fc21e98086bb29004243de1d30e10357c
chaimfrazan <chaimfrazan@gmail.com>
2026-01-12 14:43:28 +0200

    Starting a project

---
+++ src/auth/role.enum.ts
@@ -0,0 +1,6 @@
+
+export enum Role {
+  soldier = 'soldier',
+  commander = 'commander',
+}
+
---
+++ src/users/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { Role } from 'src/auth/role.enum';
+
+// This should be a real class/interface representing a user entity
+export type User = any;
+
+@Injectable()
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+      role: Role.soldier,
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+      role: Role.commander,
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
